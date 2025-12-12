0d9cfe7e722ece1e0675c87695ce1356c34b5ccc
hqureshi434 <hqureshi434@gmail.com>
2025-12-12 14:49:06 -0800

    Updated jest.config.cjs and ReviewResults.tsx

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,18 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./", // path to your Next.js app
+});
+
+const customJestConfig = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^@/(.*)$": "<rootDir>/$1",
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
+
+module.exports = createJestConfig(customJestConfig);
